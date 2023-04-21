3f9cf42a03634cccb6313895cecf8cedd082de42
GiaMadonna1 <109568865+GiaMadonna1@users.noreply.github.com>
2023-04-21 12:34:44 -0700

    Changed file name so in home so it worked

---
+++ dnj-jewelry/src/components/home.jsx
@@ -0,0 +1,18 @@
+
+function Home() {
+
+    return (
+        <div>
+            <main>
+                <div className="topnav">
+                    <a href="/">Home</a>
+                    <a href="/jewelry">Jewelry</a>
+                    <a href="/edu">Education</a>
+                    <a href="/abt">About Us</a>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default Home
