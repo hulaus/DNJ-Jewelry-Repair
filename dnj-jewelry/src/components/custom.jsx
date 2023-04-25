407b8db41b5d4ce40b4c2efede4f4a342302867d
Jo Carr <hulaus21@gmail.com>
2023-04-25 01:04:15 -0700

    added new titles and descriptions/paragraphs to cards

--- dnj-jewelry/src/components/custom.jsx
+++ dnj-jewelry/src/components/custom.jsx
@@ -18,12 +18,9 @@ function customCards() {
                     <Card className="Card" >
                         <Card.Img className="rightPicture"  src={Bracelet}/>
                         <Card.Body>
-                            <Card.Title className="introCard"> Customly Created, Expertly Crafted </Card.Title>
+                            <Card.Title className="introCard">Expertly Crafted, Uniquely Yours</Card.Title>
                                 <Card.Text className="textRight">
-                                    At DnJ Jewelry, we believe everyone should have the right to confidently display
-                                    a piece of jewelry that means something to them. Whether it's one of a kind wedding
-                                    bands or just another piece for your collection, DnJ has the tools and the expertise
-                                    to bring your vision to life. 
+                                    At DnJ Jewelry, we specialize in creating custom jewelry that is expertly crafted to reflect your unique style. From one-of-a-kind wedding bands to cherised pieces for your collection we have the tools and expertise to bring your vision to life. Trust us to deliver exceptional craftsmanship and personalized service for your custom jewelry needs.
                                 </Card.Text>
                         </Card.Body>
                     </Card>
@@ -31,18 +28,19 @@ function customCards() {
                     <Card className="Card">
                         <Card.Img className="leftPicture"  src={DangleEarrings} />
                         <Card.Body>
+                            <Card.Title>Your Dream, Our Crafstmanship</Card.Title>
                             <Card.Text>
-                                Text explaining the jewelery
-                            </Card.Text>
+                            Experience the joy of owning a truly special piece that reflects your individuality. At DNJ Jewelry, we make your dreams come true with our expert craftsmanship and personalized service. Let us bring your vision to life with our premier custom jewelry services. Trust us to deliver exceptional craftsmanship and outstanding customer service in creating your dream custom jewelry!                            </Card.Text>
                         </Card.Body>
                     </Card>
-                    <br></br>
+                    <br />
                     <Card className="Card">
                         <Card.Img className="rightPicture" src={PinUpClose} />
                         <Card.Body>
+                            <Card.Title>Crafting Your Story</Card.Title>
                             <Card.Text>
-                                Text explaining the jewelery
-                            </Card.Text>
+                            At DNJ Jewelry, we believe that every piece of jewelry tells a story. Our skilled artisans use meticulous attention to detail and over 40+ years of experience to craft custom jewelry that reflects your unique style and captures your imagination. From exquisite wedding bands to cherished pieces for your collection, our expert craftsmanship and personalized service ensure that your vision comes to life in a truly special way. Trust us to create a custom masterpiece that tells your story with our premier custom jewelry services.
+                             </Card.Text>
                         </Card.Body>
                     </Card>
                    
