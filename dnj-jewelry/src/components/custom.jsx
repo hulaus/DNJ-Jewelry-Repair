5fc90c3caeb736ada6d47064e24bc3183d005963
GiaMadonna1 <109568865+GiaMadonna1@users.noreply.github.com>
2023-04-24 17:00:03 -0700

    plz save

--- dnj-jewelry/src/components/custom.jsx
+++ dnj-jewelry/src/components/custom.jsx
@@ -1,11 +1,50 @@
 import React from "react"
+import Card from 'react-bootstrap/Card'
 
-// Custom Piece Art Gallery 
+// once the picture is in I'm thinking of changing the intro card into just a regular
+// paragraph so that it will span across the whole page and then the picture cards
+// will be underneath it. The banner image is linked in the public folder idk why it's 
+//not showing up I think i might have it linked wrong but i think it'll look neat. 
 
-function Custom() {
+function customCards() {
     return(
-    <h1>Custom Pieces</h1>
-    )
-}
+        <>
+            <img src="./public/images/custom_Jewelry.jpg" alt="DNJ Custom Jewelry"></img>
+                <div className="container">
+                    <Card className="intro" style={{ width: '18rem'}}>
+                        <Card.Img variant='top' src='holder.js/100px180? text=Image cap' />
+                        <Card.Body>
+                            <Card.Title className="introCard"> Customly Created, Expertly Crafted </Card.Title>
+                                <Card.Text>
+                                    At DnJ Jewelry, we believe everyone should have the right to confidently display
+                                    a piece of jewelry that means something to them. Whether it's one of a kind wedding
+                                    bands or just another piece for your collection, DnJ has the tools and the expertise
+                                    to bring your vision to life. 
+                                </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <br></br>
+                    <Card className="CustPic">
+                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Body>
+                            <Card.Text>
+                                Text explaining the jewelery
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <br></br>
+                    <Card className="CustPic">
+                        <Card.Img variant="top" src="holder.js/100x180" />
+                        <Card.Body>
+                            <Card.Text>
+                                Text explaining the jewelery
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+        </>) 
+        }
+            
 
-export default Custom
+
+export default customCards
--- dnj-jewelry/src/components/navbar.jsx
+++ dnj-jewelry/src/components/navbar.jsx
@@ -4,6 +4,7 @@ function Navbar() {
             <div>
                 <main>
                     <div className="topnav">
+                        <img className="logo" src="https://cdn.shopify.com/s/files/1/0075/1032/7378/f…c-4e38-8e56-805e0f7eba5e_300x300.png?v=1629414189" alt="DNJ logo"></img>
                         <a href="/">Home</a>
                         <a href="/jewelry">Jewelry</a>
                         <a href="/aboutUs">About Us</a>
