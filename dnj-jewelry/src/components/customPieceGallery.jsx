f929078953c82c3bde79c9f697cc70e16a41df39
Jo Carr <hulaus21@gmail.com>
2023-04-26 18:15:44 -0700

    making more style changes to picture gallery

--- dnj-jewelry/src/components/customPieceGallery.jsx
+++ dnj-jewelry/src/components/customPieceGallery.jsx
@@ -5,6 +5,7 @@ function Customwork() {
     return(
         <>
         <div>
+            <h1>Custom Work Gallery</h1>
         <img className="banner" src={customJewelry} alt="DNJ Jewelry Banner" />
         </div>
         <div>
