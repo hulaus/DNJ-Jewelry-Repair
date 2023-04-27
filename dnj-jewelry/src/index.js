a53d7eb3796957eedb276c90c5975b73e179bd59
Jo Carr <hulaus21@gmail.com>
2023-04-26 21:26:57 -0700

    reinstalled react-bootsrap and fixed minor bugs

--- dnj-jewelry/src/index.js
+++ dnj-jewelry/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Client from 'shopify-buy';
 
 const client = Client.buildClient({ storefrontAccessToken: 'your-access-token', domain: 'your-shopify-url.myshopify.com' });
 
