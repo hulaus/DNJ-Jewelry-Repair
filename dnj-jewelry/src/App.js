5579c7ba33da33a51f9d7d9e8b08f09a7a7caff6
Jo Carr <hulaus21@gmail.com>
2023-04-27 12:16:40 -0700

    initial startup for shopify API and storefront

--- dnj-jewelry/src/App.js
+++ dnj-jewelry/src/App.js
@@ -6,8 +6,35 @@ import Forum from './components/forum';
 import Aboutus from './components/aboutUs';
 import Custom from './components/custom';
 import Navbar from './components/navbar';
+import Cart from './components/shopify/cart'
+import { useState } from 'react';
+import { connect } from 'react-redux';
+import store from './components/store';
+
 
 function App() {
+  
+  const [cartItems, setCartItems] = useState([]);
+
+  const updateQuantityInCart = (lineItemId, quantity) => {
+  // update the quantity of a line item in the carItems array 
+    const state = store.getState(); // State from redux store
+    
+
+    setCartItems(updatedCartItems);
+  }
+
+  const removeLineItemInCart = (lineItemId) => {
+    //remove a line item from the cartItems arry
+    setCartItems(updatedCartItems);
+  }
+
+  const handleCartClose = () => {
+    // close the cart by setting isCartOpen to false
+
+  }
+
+
   return (
     <div className="App">
    <Navbar />
@@ -20,9 +47,17 @@ function App() {
       <Route path="Custom" element={<Custom />} />
     </Routes>
    </BrowserRouter>
+   <Cart 
+      checkout={this.state.checkout}
+      isCartOpen={this.state.isCartOpen}
+      handleCartClose={this.handleCartClose}
+      updateQuantityInCart={this.updateQuantityInCart}
+      removeLineItemInCart={this.removeLineItemInCart}
+   />
     </div>
 
   );
 }
 
-export default App;
+//connects Redux state to the App Component
+export default connect((state) => state)(App);
--- dnj-jewelry/src/index.js
+++ dnj-jewelry/src/index.js
@@ -4,15 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Client from 'shopify-buy';
+import { Provider } from 'react-redux';
+import store from './store'
 
 const client = Client.buildClient({ storefrontAccessToken: 'your-access-token', domain: 'your-shopify-url.myshopify.com' });
+store.dispatch({type: 'CLIENT_CREATED', payload: client});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+// buildClient() is synchronous, so we can call all these after!
+client.product.fetchAll().then((res) => {
+  store.dispatch({type: 'PRODUCTS_FOUND', payload: res});
+ });
+ client.checkout.create().then((res) => {
+  store.dispatch({type: 'CHECKOUT_FOUND', payload: res});
+ });
+ client.shop.fetchInfo().then((res) => {
+  store.dispatch({type: 'SHOP_FOUND', payload: res});
+ });
+
+ReactDOM.render(
+  <Provider store={store}>
+     <IntlProvider locale={locale} messages={flattenMessages(messages[locale.substring(0, 2)])}>
+       <App locale={locale}/>
+     </IntlProvider>
+  </Provider>,
+  document.getElementById('root')
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
