a6f706c0321f2e0f8139bc921ca03c479301834e
GiaMadonna1 <109568865+GiaMadonna1@users.noreply.github.com>
2023-04-28 12:43:39 -0700

    Basic Card. Next Step make table for blog posts + write code to populate

--- dnj-jewelry/src/components/forum.jsx
+++ dnj-jewelry/src/components/forum.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
 
 function Forum() {
     return(
+        <div className="forumWrap">
         <h1>The Jewelry Fix</h1>
+            <Card className="forumCards" border="secondary" style={{ width: '18rem'}}>
+                <Card.Header className="forumHead"> Featured </Card.Header>
+                <Card.Body className="forumBody">
+                    <Card.Title className="forumTitle"> Special Title Treatment </Card.Title>
+                    <Card.Text className="forumText">
+                        On the grind
+                    </Card.Text>
+                    <Button variant="primary">Go Somewhere</Button>
+                </Card.Body>
+            </Card>
+        </div>
     )
 }
 
