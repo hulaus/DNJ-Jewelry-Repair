8646f038d5f14de227057fbca89f77d0ed4ac938
GiaMadonna1 <109568865+GiaMadonna1@users.noreply.github.com>
2023-05-01 16:06:11 -0700

    Tried something new. Also didn't work, saving to keep track

--- dnj-jewelry/src/App.js
+++ dnj-jewelry/src/App.js
@@ -2,30 +2,32 @@ import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './components/home';
 import Jewelry from './components/jewelry';
+import Forum from './components/forum';
 import Aboutus from './components/aboutUs';
 import Custom from './components/custom';
 import Navbar from './components/navbar';
-import ImageGallery from './components/ImageGallery';
 
+
+
+export const baseUrl = 'http:localhost:5000'
 
 function App() {
+
   return (
-    <>
     <div className="App">
-      <header>
    <Navbar />
    <BrowserRouter>
+   <useWindowSize />
     <Routes>
       <Route path="/" element={<Home />}/>
       <Route path="jewelry" element={<Jewelry />} />
-      <Route path="ImageGallery" element={<ImageGallery />} />
+      <Route path="forum" element={<Forum />} />
       <Route path="Aboutus" element={<Aboutus />} />
       <Route path="Custom" element={<Custom />} />
     </Routes>
    </BrowserRouter>
-   </header>
     </div>
-    </>
+
   );
 }
 
