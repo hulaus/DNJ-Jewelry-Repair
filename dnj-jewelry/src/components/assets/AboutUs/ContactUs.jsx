19b0c581651fd2e77736a0c784a76485566e1b93
Jo Carr <hulaus21@gmail.com>
2023-04-30 21:48:07 -0700

    added new color to cards, Logo, aligned Cards Horizontally and started Contact Us Section

---
+++ dnj-jewelry/src/components/assets/AboutUs/ContactUs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Contactus(){
+    return(
+        <>
+        <h3 className="ContactUsHeader">Contact Us </h3>
+        </>
+    )
+}
+
+export default Contactus
--- dnj-jewelry/src/components/assets/AboutUs/aboutUsCards.jsx
+++ dnj-jewelry/src/components/assets/AboutUs/aboutUsCards.jsx
@@ -15,7 +15,7 @@ function AboutUsCards() {
                         Expert Craftsman You Can Trust
                     </Card.Title>
                     <Card.Img className="CardImage" variant="top" src={Bench} alt="Jeweler At Bench" />
-                    <Card.Text>
+                    <Card.Text className="TextInCard">
                         With more than 30 Years of experience as goldsmiths and jewelry repair specialists, our master craftsmen are skilled in providing meticulous attention to detail and unmatched quality.
                         You can Trust us to handle your precious jewelry with the utmost care and expertise.
                     </Card.Text>
@@ -27,7 +27,7 @@ function AboutUsCards() {
                         Why Choose Us?
                     </Card.Title>
                     <Card.Img className="CardImage" variant="top" src={SettingDiamond} alt="Jeweler Setting Diamond on Eternity Style Ring." />
-                    <Card.Text>
+                    <Card.Text className="TextInCard">
                         Our Reputation as a trusted jeweler speaks for itself.
                         For over 30 years, we've served as the go to jewelry experts for well-known names in the Las Vegas area, including MGM Jewelry Store, Gabriel Jewelers, Ultra Diamonds, MJ Christensens, Na Hoku, Miner's Gems and Cash America (Super Pawn).
                         When you trust us with your precious jewelry, you can be confident that it's in the hands of experienced, skilled craftsmen who take pride in their work!
@@ -40,7 +40,7 @@ function AboutUsCards() {
                         Celebrating Life Through Jewelry
                     </Card.Title>
                     <Card.Img className="CardImage" variant="top" src={WeddingPhoto} />
-                    <Card.Text>
+                    <Card.Text className="TextInCard">
                         We Believe in Celebrating life's precious moments, big or small.
                         Our mission is to be there for our customers, offering unique jewelry pieces that help create meaningful connections with loved ones.
                         We strive to inspire love, happiness, and friendships through our collection, and help our customers make memories that last a lifetime.
