b8a68487ac7f72fb61e35679d77fa08d7d2ea734
Jo Carr <hulaus21@gmail.com>
2023-05-01 16:53:27 -0700

    fixed bug in custom photo gallery and will be moving to new branch to finish gallery

--- dnj-jewelry/src/components/ImageGallery.jsx
+++ dnj-jewelry/src/components/ImageGallery.jsx
@@ -1,73 +1,73 @@
-import React,{useState} from "react";
-import Masonry, { ResponsiveMasonry }from "react-responsive-masonry";
+import React, { useState } from "react";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
-const images = [
-    "https://picsum.photos/100/200",
-    "https://picsum.photos/200/300",
-    "https://picsum.photos/100/300",
-    "https://picsum.photos/300/150",
-    "https://picsum.photos/200/300",
-    "https://picsum.photos/100/200",
-    "https://picsum.photos/250/350",
-    "https://picsum.photos/200/300",
-]
+const images = [  "https://picsum.photos/100/200",  "https://picsum.photos/200/300",  "https://picsum.photos/100/300",  "https://picsum.photos/300/150",  "https://picsum.photos/200/300",  "https://picsum.photos/100/200",  "https://picsum.photos/250/350",  "https://picsum.photos/200/300",];
 
 const ImageGallery = () => {
-const [data, setData] = useState({img: '', i:0})
+  const [data, setData] = useState({ img: "", i: 0 });
 
-const viewImage = (img, i) => {
-    setData({img, i})
-}
+  const viewImage = (img, i) => {
+    setData({ img, i });
+  };
 
-const imgAction = (action) => {
+  const imgAction = (action) => {
     let i = data.i;
-    if(action === 'next-img'){
-        setData({img: images[i + 1], i: i + 1});
+    if (action === "next-img") {
+      setData({ img: images[i + 1], i: i + 1 });
     }
-    if(action === 'previous-img'){
-        setData({img: images[i - 1], i: i - 1});
-}
+    if (action === "previous-img") {
+      setData({ img: images[i - 1], i: i - 1 });
+    }
+  };
 
-   return(
-<>
-{data.img &&
-<div style={{
-    width: '100%',
-    height: '100vh',
-    background: 'black',
-    position: 'fixed',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    overflow: 'hidden',
-}}>
-    <button style={{position: 'absolute', top: '10px', right:'10px'}}>X</button>
-    <button onClick={() => imgAction('previous-img')}> Previous </button>
-    <img src={data.img} style={{width: 'auto', maxWidth: '90%', maxHeight: '90%'}} alt=""/>
-    <button onClick={() => imgAction('next-img')}> Next </button>
-</div>
-
-}
-<div style={{padding:'10px'}}>
-<ResponsiveMasonry
-    columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
-    >
-        <Masonry>
-            {images.map((image, i) =>(
-                <img 
+  return (
+    <>
+      {data.img && (
+        <div
+          style={{
+            width: "100%",
+            height: "100vh",
+            background: "black",
+            position: "fixed",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            overflow: "hidden",
+          }}
+        >
+          <button style={{ position: "absolute", top: "10px", right: "10px" }}>
+            X
+          </button>
+          <button onClick={() => imgAction("previous-img")}> Previous </button>
+          <img
+            src={data.img}
+            style={{ width: "auto", maxWidth: "90%", maxHeight: "90%" }}
+            alt=""
+          />
+          <button onClick={() => imgAction("next-img")}> Next </button>
+        </div>
+      )}
+      <div style={{ padding: "10px" }}>
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+          <Masonry>
+            {images.map((image, i) => (
+              <img
                 key={i}
                 src={image}
-                style={{width: "100%", display: "block", cursor:"pointer"}}
+                style={{
+                  width: "100%",
+                  display: "block",
+                  cursor: "pointer",
+                }}
                 alt=""
                 onClick={() => viewImage(image, i)}
-                />
+              />
             ))}
-        </Masonry>
-    </ResponsiveMasonry>
-</div>
-</>
-   )
-}
-}
+          </Masonry>
+        </ResponsiveMasonry>
+      </div>
+    </>
+  );
+};
 
-export default ImageGallery
+export default ImageGallery;
