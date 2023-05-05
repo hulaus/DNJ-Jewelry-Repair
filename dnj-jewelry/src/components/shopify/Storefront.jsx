6315c40f49f059a45537b3b5b88b096f374c327f
GiaMadonna1 <109568865+GiaMadonna1@users.noreply.github.com>
2023-05-05 11:23:53 -0700

    Changed nav so on resize it's a hamburger menu

--- dnj-jewelry/src/components/shopify/Storefront.jsx
+++
@@ -1,7 +0,0 @@
-import {createStorefrontClient} from '@shopify/hydrogen-react';
-
-export const client = createStorefrontClient({
-    storeDomain:'https://{DnJ-Jewelry}.myshopify.com',
-    storefrontApiVersion: '2023-01',
-    privateStorefrontToken: `process.env.PRIVATE_STORE_TOKEN`
-})
